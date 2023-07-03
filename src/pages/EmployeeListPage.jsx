14c853a7ee50cd3fd1e93ff0243c30b9dc9ae34b
Lucie Caillat <lucie.caillat@gmail.com>
2023-07-03 19:46:03 +0200

    add search bar

--- src/pages/EmployeeListPage.jsx
+++ src/pages/EmployeeListPage.jsx
@@ -1,15 +1,38 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Table } from "antd";
+import { Table, Input } from "antd";
 import columns from "../utils/columns";
+import { useState } from "react";
+const { Search } = Input;
 
 export default function EmployeeListePage() {
   const dataSource = useSelector((state) => state.employees);
+  const [data, setData] = useState(dataSource);
+
+  const search = (e) => {
+    const value = e.target.value.toUpperCase();
+    const newData = dataSource.filter((employee) => {
+      const allEntries = Object.entries(employee)
+        .map((entrie) => entrie[1])
+        .join(" ")
+        .toUpperCase();
+      return allEntries.includes(value);
+    });
+    setData(newData);
+  };
 
   return (
     <div className="container">
       <h1>Current Employees</h1>
-      <Table dataSource={dataSource} columns={columns} rowKey={"id"} />
+
+      <Search
+        placeholder="Search"
+        onChange={search}
+        style={{
+          width: 200,
+        }}
+      />
+      <Table dataSource={data} columns={columns} rowKey={"id"} />
       <Link to="/">Home</Link>
     </div>
   );
